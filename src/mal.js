1098df904b188b6fe4c84db2fea1790135ecfd22
Jewel Mahanta <jewelmahanta@gmail.com>
2017-10-23 00:44:13 +0530

    add value check for parameter type in getUserList

--- src/mal.js
+++ src/mal.js
@@ -1,16 +1,19 @@
+// TODO: compatibility with browser
 const fetch = require("node-fetch");
 const { promisify } = require("util");
 const parseString = promisify(require("xml2js").parseString);
-
 
 const MyAnimeList = function (username, password) {
     this.username = username;
     this.password = password;
 };
 
-MyAnimeList.prototype.getUserList = async function () {
+MyAnimeList.prototype.getUserList = async function (type="anime") {
+    if (type !== "anime" && type !== "manga") {
+        throw new Error("Argument type should be 'anime' or 'manga' only.");
+    }
     try {
-        const endpoint = `https://myanimelist.net/malappinfo.php?u=${this.username}&status=all&type=anime`;
+        const endpoint = `https://myanimelist.net/malappinfo.php?u=${this.username}&status=all&type=${type}`;
         const response = await fetch(endpoint);
         if (!response.ok) {
             throw new Error(response.status.toString());
